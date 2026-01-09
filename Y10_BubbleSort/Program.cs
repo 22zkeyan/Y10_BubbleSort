762d7595830fd726c48f6847dc27dcafc535418d
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2026-01-09 11:33:23 +0000

    Add project files.

---
+++ Y10_BubbleSort/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Y10_BubbleSort
+{
+    internal class Program
+    {
+        static void Starter()
+        {
+            int[] ages = { 12, 16, 14, 15 };
+            int search_key = 16;
+            foreach (int age in ages) 
+            {
+                if (age == search_key)
+                {
+                    Console.WriteLine($"Search key found at index {Array.IndexOf(ages, age)}");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Starter();
+        }
+    }
+}
