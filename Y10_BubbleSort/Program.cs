1bde6e0cae21905b2662672d0a59d9cdd79f5f82
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2026-01-12 16:32:14 +0000

    finished bubble sort

--- Y10_BubbleSort/Program.cs
+++ Y10_BubbleSort/Program.cs
@@ -14,25 +14,30 @@
                 }
             }
         }
-        static void OptimisedBubbleSort(int[] array) //this uses a swapped flag to allow early exit, and after each loop of the array, it decreases the length by 1, as the last element will have been sorted
+        static void OptimisedBubbleSort(int[] array) 
+        //this uses a swapped flag to allow early exit, and after each loop of the array, it decreases the length by 1, as the last element will have been sorted
         {
             int length = array.Length;
             while (length > 1)
             {
                 int last_swap = 0;
-                for (int i = 0; i < length-2; i++)
+                for (int i = 0; i < length-2; i++) //don't need to go all the way to the last element in the array because that will automatically be sorted with everything else
                 {
-                    if (array[i] > array[i + 1])
+                    if (array[i] > array[i + 1]) //if the term on the left is bigger than on the right (not sorted)
                     {
                         int temp = array[i];
-                        array[i] = array[i + 1];
+                        array[i] = array[i + 1]; //set the current element to the one to the right
+                        array[i + 1] = temp;
+                        last_swap = i + 1;
                     }
                 }
+                length = last_swap;
             }
         }
         static void Main(string[] args)
         {
-            Starter();
+            int[] array = { 2,7,6,12,16,19,32,1};
+            OptimisedBubbleSort(array);
         }
     }
 }
